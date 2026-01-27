663087cc50f68c438686d301cefc36d7008ab16f
Alex <romian37@gmail.com>
2026-01-27 10:17:22 +0700

    Removed unused constructor

--- src/main/java/com/tokopakde/toryment/pojo/TransactionDetail.java
+++ src/main/java/com/tokopakde/toryment/pojo/TransactionDetail.java
@@ -1,13 +1,11 @@
 package com.tokopakde.toryment.pojo;
 
 import com.tokopakde.toryment.model.company.Product;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class TransactionDetail {
     private Product product;
     private Integer quantity;
