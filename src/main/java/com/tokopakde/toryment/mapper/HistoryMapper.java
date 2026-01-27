1a7ea02c9e9d32b8905ed32ebe96b7ee6edd6990
Alex <romian37@gmail.com>
2026-01-27 10:16:52 +0700

    rename to be me proper

--- src/main/java/com/tokopakde/toryment/mapper/HistoryMapper.java
+++ src/main/java/com/tokopakde/toryment/mapper/HistoryMapper.java
@@ -6,5 +6,5 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface HistoryMapper {
-    HistoryResDTO toDto(History history);
+    HistoryResDTO mapToDto(History history);
 }
